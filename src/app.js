44cceb2bd27829041ac7d3d7f7c18a7139810baa
rkrasiy <krasiyruslan@gmail.com>
2023-04-18 11:10:03 +0200

    start

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var message = 'Hello, Typescript 1!';
+// create a new heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello, Typescript 1!';
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
